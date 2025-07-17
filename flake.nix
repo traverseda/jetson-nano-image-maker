af4a07d81e1cead2ad6c350c79500156404c5d52
Alex Davies <traverse.da@gmail.com>
2025-07-17 11:18:20 -0300

    Update artifact version

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            libxml2
+          ];
+          shellHook = ''
+            export JETSON_ROOTFS_DIR=/tmp/jetson-builder/rootfs
+            export JETSON_BUILD_DIR=/tmp/jetson-builder/build
+          '';
+        };
+      });
+    };
+}
